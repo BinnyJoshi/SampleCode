b309bc11dee2995e6b8610d6717e72b24ad99ab3
binny <binny@192.168.1.2>
2021-03-08 08:34:38 -0800

    need solution

---
+++ selenium-java-dropdowns-and-checkboxes/src/main/java/com/hackerrank/selenium/OnlineSurvey.java
@@ -0,0 +1,29 @@
+package com.hackerrank.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class OnlineSurvey {
+
+	@FindBy(xpath = "//a[contains(text(),'Deals')]")
+	WebElement dropDown;
+	
+	
+    public static void fillDateOfBirth(WebDriver driver, String pageUrl) {
+        // complete implementation
+    
+    }
+
+    public static void answerQuestions(WebDriver driver, String pageUrl) {
+        // complete implementation
+    	
+    }
+    //tHIS IS JUST TO PUSH CODE IN GITHUB 
+    public static void xyz()
+    {
+    	
+    }
+    
+}
